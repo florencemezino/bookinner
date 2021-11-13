3461b33f63ccb6ddc66e0a7f69d27d4576310ee9
Florence Mezino <florence.mezino@outlook.com>
2021-11-13 19:12:40 +0000

    Connect to Mongodb

---
+++ mongo_project.py
@@ -0,0 +1,112 @@
+import os
+import pymongo
+if os.path.exists("env.py"):
+    import env
+
+
+MONGO_URI = os.environ.get("MONGO_URI")
+DATABASE = "myFirstDB"
+COLLECTION = "celebrities"
+
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
+
+
+def add_book():
+    print("")
+    title = input("Enter book title > ")
+    author = input("Enter author name > ")
+    description = input("Enter a description > ")
+    stars = input("Select number of stars > ")
+    amazon_link = input("Enter your affiliate link > ")
+    image_url = input("Add the book image > ")
+    category = input("Enter a category > ")
+
+    new_doc = {
+        "title": first.lower(),
+        "author": last.lower(),
+        "description": description,
+        "stars": stars,
+        "amazon_link": amazon_link,
+        "image_url": image_url,
+        "category ": category
+    }
+
+    try:
+        coll.insert(new_doc)
+        print("")
+        print("Document inserted")
+    except:
+        print("Error accessing the database")
+
+
+def edit_book():
+    doc = get_book()
+    if doc:
+        update_doc = {}
+        print("")
+        for k, v in doc.items():
+            if k != "_id":
+                update_doc[k] = input(k.capitalize() + " [" + v + "] > ")
+
+                if update_doc[k] == "":
+                    update_doc[k] = v
+
+        try:
+            coll.update_one(doc, {"$set": update_doc})
+            print("")
+            print("Document updated")
+        except:
+            print("Error accessing the database")
+
+
+def delete_book():
+    doc = get_book()
+    if doc:
+        print("")
+        for k, v in doc.items():
+            if k != "_id":
+                print(k.capitalize() + ": " + v.capitalize())
+
+        print("")
+        confirmation = input("Is this the document you want to delete?\nY or N > ")
+        print("")
+
+        if confirmation.lower() == "y":
+            try:
+                coll.remove(doc)
+                print("Document deleted!")
+            except:
+                print("Error accessing the database")
+        else:
+            print("Document not deleted")
+
+
+def main_loop():
+    while True:
+        option = show_menu()
+        if option == "1":
+            add_record()
+        elif option == "2":
+            find_record()
+        elif option == "3":
+            edit_record()
+        elif option == "4":
+            delete_record()
+        elif option == "5":
+            conn.close()
+            break
+        else:
+            print("Invalid option")
+        print("")
+
+
+conn = mongo_connect(MONGO_URI)
+coll = conn[DATABASE][COLLECTION]
+main_loop()
